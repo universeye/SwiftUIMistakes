4682c9d12be62bc62f431e7be0c3f4c36b7e97ae
Terry <terrykuo501@gmail.com>
2021-03-24 23:02:14 +0800

    Initial Commit

--- SwiftUIMistakes/ContentView.swift
+++ SwiftUIMistakes/ContentView.swift
@@ -9,13 +9,26 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        TabView {
+            Text("Hello, world!")
+                .padding()
+                .tabItem {
+                    SFSymbols.home
+                    Text("Home")
+                }
+        
+        Text("2")
+            .tabItem { SFSymbols.slider
+                Text("Slider")
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.light)
     }
 }
---
+++ SwiftUIMistakes/SFSymbols.swift
@@ -0,0 +1,20 @@
+//
+//  SFSymbols.swift
+//  SwiftUIMistakes
+//
+//  Created by Terry Kuo on 2021/3/24.
+//
+
+import Foundation
+import SwiftUI
+
+enum SFSymbols: String, View {
+    case home = "homekit"
+    case slider = "slider.horizontal.below.rectangle"
+    case strokeShape = "squareshape.dashed.squareshape"
+    case alert = "exclamationmark.triangle.fill"
+    
+    var body: some View {
+        Image(systemName: self.rawValue)
+    }
+}
