aada91dfeaccf857ebe57c6465d8c13c323d9cc3
Terry <terrykuo501@gmail.com>
2021-03-25 11:02:47 +0800

    Extension to Binding

--- SwiftUIMistakes/SliderView.swift
+++ SwiftUIMistakes/SliderView.swift
@@ -12,10 +12,33 @@ struct SliderView: View {
     @State private var rating = 0.0
     
     var body: some View {
-        Slider(value: $rating)
-            .onChange(of: rating, perform: { value in
-                print("Rating changed to \(value)")
-            })
+        Slider(value: $rating.onChange(sliderChanged))
+            
+//            .onChange(of: rating, perform: { value in
+//                print("Rating changed to \(value)")
+//            })
+    }
+    
+    
+    func sliderChanged(_ value: Double) {
+        print("Rating changed to \(value)")
+    }
+}
+
+//extension on binding with function onChange
+extension Binding {
+    //escaping over whatever the value of the binding is, returns Void, returns a new Binding over that value
+    func onChange(_ handler: @escaping (Value) -> Void) -> Binding<Value> {
+        //return a Binding
+        Binding(
+            //where the getter is
+            get: { self.wrappedValue },
+            //where the setter is
+            set: { newValue in
+                self.wrappedValue = newValue
+                handler(newValue)
+            }
+        )
     }
 }
 
