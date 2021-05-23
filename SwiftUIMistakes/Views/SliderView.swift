2404e3feb95add3c7a6c26c6403a00e119943cb5
Terry <terrykuo501@gmail.com>
2021-05-23 13:39:40 +0800

    add .gitignore file

--- SwiftUIMistakes/Views/SliderView.swift
+++ SwiftUIMistakes/Views/SliderView.swift
@@ -10,10 +10,18 @@ import SwiftUI
 struct SliderView: View {
     
     @State private var rating = 0.0
+    @State private var rating2 = 0.0
     
     var body: some View {
-        Slider(value: $rating.onChange(sliderChanged))
-            
+        VStack {
+            Slider(value: $rating.onChange({ value in
+                sliderChanged(value)
+            }))
+        
+        
+        //another way of writing above's code is:
+        Slider(value: $rating2.onChange(sliderChanged(_ :)))
+        }
 //            .onChange(of: rating, perform: { value in
 //                print("Rating changed to \(value)")
 //            })
